8086d35b66dd155ea42ca3e86656438075fb84e8
EkaakshaAsija01 <ea8275@pleasantonusd.net>
2025-11-14 22:11:33 +0000

    Started the Card class and spent the period pulling and setting up the project.

--- cards/Card.java
+++ cards/Card.java
@@ -1,6 +1,10 @@
 package cards;
 
 public class Card {
+private int value;
+private int suit;
+private String[] VALUE = {"A","2", "3","4","5","6","7","8","9","10","J","Q","K"}
+private String[] SUIT = {""}
 
 
 }
