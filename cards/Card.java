d89d93b12321cfc5573f395dd6adc2e971ebdd14
EkaakshaAsija01 <ea8275@pleasantonusd.net>
2025-11-18 04:25:56 +0000

    I finished my card Class. Im going to eat dinner and start another class. I didn't commit in class because I was doing the packet. I will probably finish the Deck or Hand Class soon

--- cards/Card.java
+++ cards/Card.java
@@ -3,8 +3,20 @@ package cards;
 public class Card {
 private int value;
 private int suit;
-private String[] VALUE = {"A","2", "3","4","5","6","7","8","9","10","J","Q","K"}
-private String[] SUIT = {""}
-
+private String[] SUITS = {"♦","♣","♥","♠"}
+private String[] VALUES = {"A","2", "3","4","5","6","7","8","9","10","J","Q","K"}
+public Card(int suit, int value)
+{
+    this.suit=suit;
+    this.value=value;
+}
+public int getValue()
+{
+    return value;
+}
+public String toString()
+{
+    return ""+ SUITS[suit]+VALUES[value];
+}
 
 }
