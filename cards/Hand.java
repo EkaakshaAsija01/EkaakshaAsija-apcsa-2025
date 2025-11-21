cabc05842443635b056b6f56e99642965af3a607
EkaakshaAsija01 <ea8275@pleasantonusd.net>
2025-11-21 03:57:39 +0000

    I added the comments to the hand class. I forgot to add them earlier. I made no progress yesterday because I was studying for the apclassroom test.

--- cards/Hand.java
+++ cards/Hand.java
@@ -1,48 +1,55 @@
 package cards;
 
 public class Hand {
-    private Card[] cards;
-    private int length;
+    private Card[] cards; // holds all the cards
+    private int length;   // current card count
 
     public Hand(int maxCards) {
-        cards = new Card[maxCards];
-        length = 0;
+        cards = new Card[maxCards]; // make card array
+        length = 0; // start with zero
     }
 
     public void add(Card card) {
+        // add card if space
         if (length < cards.length) {
-            cards[length] = card;
-            length++;
+            cards[length] = card; // place card here
+            length++; // increase count
         }
     }
 
     public int length() {
-        return length;
+        return length; // return card amount
     }
 
     public Card get(int index) {
+        // get card by index
         if (index >= 0 && index < length) {
-            return cards[index];
+            return cards[index]; // return this card
         }
-        return null;
+        return null; // invalid index case
     }
 
     public Card remove(int index) {
+        // remove card at index
         if (index < 0 || index >= length) return null;
-        Card removed = cards[index];
+        Card removed = cards[index]; // save removed card
+
+        // shift cards leftwards
         for (int i = index; i < length - 1; i++) {
-            cards[i] = cards[i + 1];
+            cards[i] = cards[i + 1]; // move card left
         }
-        cards[length - 1] = null;
-        length--;
-        return removed;
+
+        cards[length - 1] = null; // clear last spot
+        length--; // reduce card count
+        return removed; // return removed card
     }
 
     public String toString() {
+        // convert hand to string
         String result = "";
         for (int i = 0; i < length; i++) {
-            result += cards[i];
-            if (i < length - 1) result += " ";
+            result += cards[i]; // add card text
+            if (i < length - 1) result += " "; // add space here
         }
         return result;
     }
