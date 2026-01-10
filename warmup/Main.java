160ebf08495e2dd14f0d91d72d38713801a57437
EkaakshaAsija01 <ea8275@pleasantonusd.net>
2026-01-10 02:05:14 +0000

    This is the answer to number 1.  I am going to delete to replace with a different question.

--- warmup/Main.java
+++ warmup/Main.java
@@ -3,7 +3,16 @@ package warmup;
 public class Main {
 
     public static void main(String[] args) {
-        // Your code here.
+       int sum=0;
+       int i=0;
+      while(i<0){
+
+        if(i%3==0 || i%5==0){
+          sum+=i;
+          i++;
+        }
+      }
+      System.out.println(sum);
 
     }
 }
