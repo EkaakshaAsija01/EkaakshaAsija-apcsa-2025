b58d1ab6dea22f08052e9a7e10354215e07fa0af
EkaakshaAsija01 <ea8275@pleasantonusd.net>
2026-01-11 21:09:51 +0000

    I don't know why my commit for the fibbonacci sequence didn't go through. It showed as commited and synced friday but when i checked today, it wasn't here. I am just recommiting what I said friday. "This is the answer to number 2. I used a while loop to go through the limit and an if statement to check if the number was even or odd and then add it to the sum."

--- warmup/Main.java
+++ warmup/Main.java
@@ -3,14 +3,17 @@ package warmup;
 public class Main {
 
     public static void main(String[] args) {
-       int sum=0;
-       int i=0;
-      while(i<0){
-
-        if(i%3==0 || i%5==0){
-          sum+=i;
-          i++;
+      int lastNum=4000000;
+      int a=1;
+      int b=2;
+      int sum=0;
+      while (a<=lastNum){
+        if(a%2==0){
+          sum+=a;
         }
+        int NextVal=a+b;
+        a=b;
+        b=NextVal;
       }
       System.out.println(sum);
 
