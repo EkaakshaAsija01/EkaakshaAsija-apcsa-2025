4e8d643729318804e42d04d3e9e2ba30113a1501
EkaakshaAsija01 <ea8275@pleasantonusd.net>
2026-01-30 05:39:51 +0000

    I finished both classes that we had to finish. I already had the bones and the structure done during class and added any final touches. The test suite is now passing and I will finish the Doc tomorrow. The reason I didn't do anything on Wednesday was because we had the group quiz and the FRQ.

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -4,5 +4,16 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
+        for (int i = 1; i < input.length; i++) {
+            int key = input[i]; // Current element
+            int j = i - 1;
+
+            while (j >= 0 && input[j] > key) {
+                input[j + 1] = input[j]; // Shift right
+                j--; // Move left
+            }
+
+            input[j + 1] = key; // Insert element
+        }
     }
-}
+} //Explanation in the comments I put. 
--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -3,6 +3,27 @@ package sorting;
 public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
+        System.out.println("Selection Sort!!!"); 
+
+        // Loop through each element
+        for (int i = 0; i < input.length - 1; i++) {
+            int minIndex = i; // Assume current is smallest
+
+            // Find smallest element in remaining array
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[j] < input[minIndex]) {
+                    minIndex = j; // Update smallest index
+                }
+            }
+
+            // Swap smallest with current element
+            int temp = input[minIndex];
+            input[minIndex] = input[i];
+            input[i] = temp;
+            
+        }
     }
 }
+//Summary: Selection sort basically assigns a random (first) value as the minimum. 
+//It then traverses through the array to find anything smaller and switches the values if one is present.
+// It keeps going for each term.
