e22b6212645277b44efce8af94734735d05fa14b
EkaakshaAsija01 <ea8275@pleasantonusd.net>
2026-03-02 18:56:16 +0000

    deleted perimeter method

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -19,38 +19,7 @@ public class IrregularPolygon {
      public double perimeter() {
         // TODO: Calculate the perimeter.
 
-        // If there are fewer than 2 points, there is no perimeter
-        if (myPolygon.size() < 2) {
-            return 0;
-        }
-
-        double total = 0;
-
-        // Loop through each point in the polygon
-        for (int i = 0; i < myPolygon.size(); i++) {
-            Point2D.Double current = myPolygon.get(i);
-
-            // For the last point, connect it back to the first point
-            Point2D.Double next;
-            if (i == myPolygon.size() - 1) {
-                next = myPolygon.get(0); // connect last point to first
-            } else {
-                next = myPolygon.get(i + 1);
-            }
-
-            // Calculate distance between current point and next point
-            double distX = next.x - current.x;
-            double distY = next.y - current.y;
-            total += Math.sqrt(distX * distX + distY * distY);
-        }
-
-        return total;
-    }
-
-    public double area() {
-        // TODO: Calculate the area.
-        Double area = 0.0;
-        return area;
+       
     }
 
     public void draw()
